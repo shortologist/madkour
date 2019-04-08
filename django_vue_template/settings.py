1fad3cc9cebc6e1771117237be953870747fee58
shortologist <islamatef82@gmail.com>
2019-04-08 18:13:41 +0200

    4

--- django_vue_template/settings.py
+++ django_vue_template/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['madkour.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['madkour.herokuapp.com', 'localhost']
 
 
 # Application definition
